63119d81eee8974f5ee33654c9004862f165fd5f
unknown <jorginnenad@gmail.com>
2021-04-22 09:23:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import uuid from 'uuid/v4';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+const itemsFromBackend = [
+  { id: uuid(), content: 'First task' },
+  { id: uuid(), content: 'Second task' }
+];
+
+
+  
+
+const onDragEnd = (result, columns, setColumns) => {
+   if (!result.destination) return;
+   const { source, destination } = result;
+   if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    })
+   } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    })
+   }
+}
+
 
 function App() {
+
+  const columnsFromBackend = 
+  {
+    "todo": {
+      id: uuid(),
+      name: 'To do',
+      items: []
+      },
+    "in-progress": {
+      name: 'In Progress',
+      items: []
+      },
+      "done": {
+      name: 'Done',
+      items: []
+      }  
+  };
+  
+  const [columns, setColumns] = useState(columnsFromBackend);
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+      <div>
+      <header>
+        <h1>Todo List</h1>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} todos={todos} />
+      </div>
+      <div style={{ display: 'flex' }}>
+      <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
+              <h2>{column.name}</h2>
+              <div style={{ margin: 8 }}>
+            <Droppable droppableId={id} key={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div 
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500
+                    }}
+                  >
+                    {todos.map((todo, index) => {
+                      return (
+                        <Draggable key={todo.id} draggableId={todo.id} index={index} >
+                            {(provided, snapshot) => {
+                              return (
+                                <div 
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={{
+                                    userSelect:'none',
+                                    padding: 16,
+                                    margin: '0 0 8px 0',
+                                    minHeight: '50px',
+                                    backgroundColor: snapshot.isDragging ? '#26384A' : '#456C86',
+                                    color: 'white',
+                                    ...provided.draggableProps.style
+                                  }}
+                                > 
+                                {todo.text}
+                                {/* {<TodoList setTodos={setTodos} todos={todos} />} */}
+                                
+                                </div>
+                              )
+                            }}
+                        </Draggable>
+                      )
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )
+              }}
+            </Droppable>
+            </div>
+            </div>
+          )
+        })}
+      </DragDropContext>
+      </div>
+     
     </div>
   );
 }
